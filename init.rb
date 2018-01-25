b0f7b1e53bd2c9ca80ad45fda0543d9789abfe41
Levin Gerdes <gerdeslevin@gmail.com>
2018-01-25 19:28:54 +0100

    Migrating components to ESA-PRL rover package set, added platform driver and telemetry telecommand with their dependencies

---
+++ init.rb
@@ -0,0 +1,24 @@
+def has_pcan_api?
+    include_file = File.join("/usr","include","pcan.h")
+    lib = File.join("/usr","lib","libpcan.so")
+    File.exists?(include_file) && File.exists?(lib)
+end
+
+def has_activemq_api?
+    file = File.join("/home","marta","rock","drivers","activeMQ","activemq-cpp.pc.in")
+    File.exists?(file)
+end
+
+
+#def create_metapackages
+#    Autoproj.current_package_set().each_package do |pkg|
+#        meta_name = pkg.name.split("/").first
+#        if(meta_name)
+#            metapackage("exoter.#{meta_name}", pkg.name)
+#        end
+#    end
+#end
+#
+#if Autoproj::Metapackage.method_defined?(:weak_dependencies?)
+#    metapackage('exoter').weak_dependencies = true
+#end
